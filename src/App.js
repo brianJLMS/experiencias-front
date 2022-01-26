b34be146c9140ad7ec4fc057b183eb8282b22a14
brianJLMS <brianmediansuarez@gmail.com>
2022-01-26 19:27:58 -0300

    proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { getPosts } from "./actions/posts";
+import viajes from "./img/viajes.png";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./styles";
 
 const App = () => {
+  const [currentId, setCurrentId] = useState(null);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="secondary">
+        <Typography className={classes.heading} variant="h3" align="center">
+          Travel Experiences
+        </Typography>
+        <img
+          className={classes.image}
+          src={viajes}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            className={classes.mainContainer}
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
